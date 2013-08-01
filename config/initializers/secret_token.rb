247fec3eeb3b205f8f20368eaf8f77cb3b788b65
yuki <y.shizu624@gmail.com>
2013-08-01 17:27:04 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PTodo::Application.config.secret_token = '9462b28d6d730dd12a20c70554eb40d7f6ddae0ad071a54ce79986f65fac66d03b7f25bfb468d5f6aca69e5fade12112aed870a364206c2b53dcc31c8b6a7fec'
