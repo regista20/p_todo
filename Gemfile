8f26e87cafba9b14e4fc58b27706a8dcb1db4208
yuki <y.shizu624@gmail.com>
2013-08-01 18:01:56 +0900

    Make a basic Task model

--- Gemfile
+++ Gemfile
@@ -10,7 +10,9 @@ group :development, :test do
   gem 'rspec-rails'
 end
 
-
+group :development do
+  gem 'annotate'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  content    :string(255)
+#  done       :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Task < ActiveRecord::Base
   attr_accessible :content, :done
+
+  validates :content, presence: true, length: { maximum: 50 }
 end
